81c62f5cd28d4e844bacb837adccf9a23a089d78
Gilber Moreno <6ilberM@gmail.com>
2019-01-12 15:26:16 -0500

    Improved the site very minorly

--- js/min.js
+++ js/min.js
@@ -1,7 +1,29 @@
-document.addEventListener("click", LazyLoad);
+//By 6ilberM
+//Lazy loads once
+
+window.onload = lolload;
+// document.addEventListener("click", lolload);
 
 // Consider using another one
 
-function LazyLoad() {
+function LazyLoad()
+{
     [...document.querySelectorAll('[data-src]')].map(img => img.addEventListener('click', e => img.src = img.dataset.src))
+}
+
+var DoOnce = new Boolean(false);
+ 
+function lolload()
+{
+    if (DoOnce == false) {
+        
+    var images = document.querySelectorAll('[data-src]');
+    for (var i = 0; i < images.length; i++)
+    {
+        var img = images[i];
+        img.src = img.dataset.src;
+        }
+        DoOnce = true;
+    }
+    // alert("why does this not fucking call");
 }
