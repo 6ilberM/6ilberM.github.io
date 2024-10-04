23c3eda57f07803a5bb5346ac936f173095d7d28
Gilber Moreno <6ilberm@gmail.com>
2024-10-04 05:36:38 -0500

    remove some stuff

--- src/index.tsx
+++ src/index.tsx
@@ -8,7 +8,6 @@ const Home: React.FC = () => {
         <meta charSet="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <title>6ilberM's website?</title>
-        <link rel="stylesheet" href="/styles/main.css" />
       </head>
 
       <body>
