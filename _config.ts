7811b4464479cb041b73db04a95abfc5f7718a5c
Gilber Moreno <6ilberm@gmail.com>
2024-10-04 05:55:12 -0500

    more experiments

--- _config.ts
+++ _config.ts
@@ -20,6 +20,5 @@ siteBuild.use(sass());
 siteBuild.use(codeHighlight());
 siteBuild.use(slugify_urls());
 siteBuild.use(postcss());
-siteBuild.copy("./src/styles", "styles");
 
 export default siteBuild;
