c35f5f4865bc1225d9baaabdd2caf250885d2c1a
Gilber Moreno <6ilberm@gmail.com>
2025-08-16 01:42:25 -0500

    updated a bunhc of stuff

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -2,31 +2,31 @@
 import React from "react";
 
 interface ModalProps {
-    isOpen: boolean;
-    onClose: () => void;
-    children: React.ReactNode;
+  isOpen: boolean;
+  onClose: () => void;
+  children: React.ReactNode;
 }
 
 const Modal: React.FC<ModalProps> = ({
-    isOpen,
-    onClose,
-    children,
+  isOpen,
+  onClose,
+  children,
 }: ModalProps) => {
-    if (!isOpen) return null;
+  if (!isOpen) return null;
 
-    return (
-        <div className="modal-overlay" onClick={onClose}>
-            <div
-                className="modal-content"
-                onClick={(e) => e.stopPropagation()} // Prevent closing when clicking inside
-            >
-                {children}
-                <button onClick={onClose} className="modal-close-button">
-                    Close
-                </button>
-            </div>
-        </div>
-    );
+  return (
+    <div className="modal-overlay" onClick={onClose}>
+      <div
+        className="modal-content"
+        onClick={(e) => e.stopPropagation()} // Prevent closing when clicking inside
+      >
+        {children}
+        <button onClick={onClose} className="modal-close-button">
+          Close
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default Modal;
--- src/index.tsx
+++ src/index.tsx
@@ -5,45 +5,42 @@ export const title = "Gilberto Moreno";
 export const layout = "layouts/default.tsx";
 
 const Home: React.FC = () => {
-    return (
-        <>
-            <main className="site-content">
-                <div className="profile-section">
-                    <div className="profile-container">
-                        <img
-                            src="./static/images/qxc_Squared.jpeg"
-                            alt="My Avatar"
-                            className="avatar profile-image"
-                        />
-                    </div>
-                    <div className="text-container">
-                        <h1>
-                            <span className="greeting">Hi, I'm</span>{" "}
-                            Gilberto Moreno
-                        </h1>
-                        <h2>Software Engineer & Gameplay Programmer</h2>
-                        <p>
-                            I like making things & games.
-                        </p>
-                        <p>
-                            I believe dreams shape the world, so I dream to make
-                            games people will love.
-                        </p>
-                    </div>
-                </div>
+  return (
+    <>
+      <main className="site-content">
+        <div className="profile-section">
+          <div className="profile-container">
+            <img
+              src="./static/images/qxc_Squared.jpeg"
+              alt="My Avatar"
+              className="avatar profile-image"
+            />
+          </div>
+          <div className="text-container">
+            <h1>
+              <span className="greeting">Hi, I'm</span> Gilberto Moreno
+            </h1>
+            <h2>Software Engineer & Gameplay Programmer</h2>
+            <p>
+              I like making things & games.
+            </p>
+            <p>
+              I believe dreams shape the world, so I dream to make games people
+              will love.
+            </p>
+          </div>
+        </div>
+        <a
+          className="curriculum-vitae-button"
+          href="https://drive.google.com/file/d/1-2MbmS8zoA6cRPZOukpPi7SlgJgG4L4K/view?usp=sharing"
+        >
+          Curriculum - Resume
+        </a>
 
-                <a
-                    className="curriculum-vitae-button"
-                    href="https://drive.google.com/file/d/1-2MbmS8zoA6cRPZOukpPi7SlgJgG4L4K/view?usp=sharing"
-                >
-                    Curriculum - Resume
-                </a>
-                <br />
-
-                <SupportMeWidget />
-            </main>
-        </>
-    );
+        <SupportMeWidget />
+      </main>
+    </>
+  );
 };
 
 export default Home;
