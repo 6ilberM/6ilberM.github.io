a501bf148f95d20151f6fa47237f8b449200bf49
Gilber Moreno <6ilberm@gmail.com>
2026-04-02 00:47:20 -0500

    wip

--- src/index.tsx
+++ src/index.tsx
@@ -5,44 +5,44 @@ export const title = "Gilberto Moreno";
 export const layout = "layouts/default.tsx";
 
 const Home: React.FC = () => {
-  return (
-    <>
-      <div className="profile-section">
-        <div className="profile-container">
-          <img
-            src="./static/images/qxc_Squared.jpeg"
-            alt="My Avatar"
-            className="avatar profile-image"
-          />
-        </div>
-        <div className="text-container">
-          <h1>
-            <span className="greeting">Hi, I'm</span>
-            <span className="header-name-color">
+    return (
+        <>
+            <div className="profile-section">
+                <div className="profile-container">
+                    <img
+                        src="./static/images/qxc_Squared.jpeg"
+                        alt="My Avatar"
+                        className="avatar profile-image"
+                    />
+                </div>
+                <div className="text-container">
+                    <h1>
+                        <span className="greeting">Hi, I'm</span>
+                        <span className="header-name-color">
               Gilberto Moreno
             </span>
-          </h1>
-          <h2>Software Engineer & Gameplay Programmer</h2>
-          <p>
-            I like making things & games.
-          </p>
-          <p>
-            I believe dreams shape the world, so I dream to make games people
-            will love.
-          </p>
+                    </h1>
+                    <h2>Software Engineer & Gameplay Programmer</h2>
+                    <p>
+                        I like making things & games.
+                    </p>
+                    <p>
+                        I believe dreams shape the world, so I dream to make games people
+                        will love.
+                    </p>
 
-          <a
-            className="curriculum-vitae-button"
-            href="https://drive.google.com/file/d/1-2MbmS8zoA6cRPZOukpPi7SlgJgG4L4K/view?usp=sharing"
-          >
-            Curriculum - Resume
-          </a>
-        </div>
-      </div>
+                    <a
+                        className="curriculum-vitae-button"
+                        href="https://drive.google.com/file/d/1-2MbmS8zoA6cRPZOukpPi7SlgJgG4L4K/view?usp=sharing"
+                    >
+                        Curriculum - Resume
+                    </a>
+                </div>
+            </div>
 
-      <SupportMeWidget />
-    </>
-  );
+            <SupportMeWidget/>
+        </>
+    );
 };
 
 export default Home;
